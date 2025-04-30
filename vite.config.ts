17f069f01f47f61710d856cad92859b630154cc0
Noul99 <ashrafmemorialhighschool@gmail.com>
2025-04-30 18:54:57 +0500

    Fix development server configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import autoprefixer from 'autoprefixer'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/hbl8/',
+  base: process.env.NODE_ENV === 'production' ? '/hbl8/' : '/',
   build: {
     outDir: 'docs',
   },
